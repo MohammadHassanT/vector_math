c097403328557785c2edaade373e7de8aa0fa751
MH.T <mohammad.hassan.t@hotmail.com>
2021-02-20 12:17:51 +0330

    8

--- circular_cylindrial.py
+++ circular_cylindrial.py
@@ -2,5 +2,5 @@
 from base_coordinate.BaseCoordinate import BaseCircularCylindrial
 
 class CircularCylindrial(BaseCircularCylindrial):
-    
+    pass
     
---
+++ spherial_coordinates.py
@@ -0,0 +1,4 @@
+from base_coordinate.BaseCoordinate import BaseSpherialCoordinates
+
+class SpherialCoordinates(BaseSpherialCoordinates):
+    pass 
